e570cb8d84802884e21927e7c7fd83645c448162
Josip Vulic <jovulic@gmail.com>
2025-12-02 14:55:30 -0500

    ftr: update to nixos-25.11 and use jovulic/release-please-action

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "A library for defining, executing, and persisting computation.";
 
   inputs = {
-    nixpkgs.url = "nixpkgs/nixos-25.05";
+    nixpkgs.url = "nixpkgs/nixos-25.11";
   };
 
   outputs =
